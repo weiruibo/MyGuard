4eb6afb91b0bbab3ab14f027dbe671bfa22b3853
gdmec07150835 <853671221@qq.com>
2016-12-19 15:43:25 +0800

    VersionEntity

--- app/src/main/java/cn/edu/gdmec/w07150837/myguard/m1home/entity/VisionEntity.java
+++ app/src/main/java/cn/edu/gdmec/w07150837/myguard/m1home/entity/VisionEntity.java
@@ -4,5 +4,12 @@ package cn.edu.gdmec.w07150837.myguard.m1home.entity;
  * Created by weiruibo on 12/19/16.
  */
 
-public class VisionEntity {
+class VersionEntity {
+    /*服务器版本号*/
+    public String versioncode;
+    /*版本描述*/
+    public String description;
+    /*apk下载地址*/
+    public String apkurl;
+
 }
