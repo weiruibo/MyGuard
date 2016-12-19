b54b142c0bb66df5fa1685fab7c5fa0e0aa12e0a
gdmec07150835 <853671221@qq.com>
2016-12-19 16:22:12 +0800

    VersionEntity

--- app/src/main/java/cn/edu/gdmec/w07150837/myguard/m1home/utils/MyUtils.java
+++ app/src/main/java/cn/edu/gdmec/w07150837/myguard/m1home/utils/MyUtils.java
@@ -1,8 +1,48 @@
 package cn.edu.gdmec.w07150837.myguard.m1home.utils;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+
+import java.io.File;
+
 /**
  * Created by weiruibo on 12/19/16.
  */
 
 public class MyUtils {
+    /*
+    获取版本号
+    @param context
+    @return 返回版本号
+    */
+    public static String getVersion(Context context){
+     //PackageManager 可以获取清单文件中的所有信息
+        PackageManager manager=context.getPackageManager();
+        try {
+            PackageInfo packageInfo=manager.getPackageInfo(context.getPackageName(),0);
+            return packageInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+    /*
+    安装新版本
+    @param activity
+    */
+    public static void installApk(Activity activity){
+        Intent intent=new Intent("android.intent.action.VIEW");
+        //添加默认分类
+        intent.addCategory("android.intent.category.DEFAULT");
+        //设置数据和分类 在文件中
+        intent.setDataAndType(
+                Uri.fromFile(new File("/mnt/sdcard/mobilesafe2.0.apk")),
+                "application/vnd.android.package-archive");
+        //如果开启的activity退出的时候,会调回当前activity的onActivityResult
+        activity.startActivityForResult(intent,0);
+    }
 }
