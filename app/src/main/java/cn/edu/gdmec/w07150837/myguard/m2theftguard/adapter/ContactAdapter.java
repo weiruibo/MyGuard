55573af239010ef440bf78af11e76faa3ffb7ae6
gdmec07150824 <404115235@qq.com>
2016-12-20 11:35:23 +0800

    Lys

--- app/src/main/java/cn/edu/gdmec/w07150837/myguard/m2theftguard/adapter/ContactAdapter.java
+++ app/src/main/java/cn/edu/gdmec/w07150837/myguard/m2theftguard/adapter/ContactAdapter.java
@@ -1,15 +1,65 @@
 package cn.edu.gdmec.w07150837.myguard.m2theftguard.adapter;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.content.Context;
+
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
 
 import cn.edu.gdmec.w07150837.myguard.R;
+import cn.edu.gdmec.w07150837.myguard.m2theftguard.entity.ContactInfo;
 
-public class ContactAdapter extends AppCompatActivity {
+public class
+ContactAdapter extends BaseAdapter {
+    private List<ContactInfo> contactInfos;
+    private Context context;
+
+    public ContactAdapter(List<ContactInfo> contactInfos, Context context) {
+        super();
+        this.contactInfos = contactInfos;
+        this.context = context;
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_contact_adapter);
+    public int getCount() {
+        return contactInfos.size();
     }
+
+    @Override
+    public Object getItem(int position) {
+        return contactInfos.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder = null;
+        if (convertView == null) {
+            convertView = View.inflate(context, R.layout.item_list_contact_select, null);
+            holder = new ViewHolder();
+            holder.mNameTV = (TextView) convertView.findViewById(R.id.tv_name);
+            holder.mPhoneTV = (TextView) convertView.findViewById(R.id.tv_phone);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+        holder.mNameTV.setText(contactInfos.get(position).name);
+        holder.mPhoneTV.setText(contactInfos.get(position).phone);
+        return convertView;
+    }
+
+    static class ViewHolder {
+        TextView mNameTV;
+        TextView mPhoneTV;
+    }
+
 }
--- app/src/main/java/cn/edu/gdmec/w07150837/myguard/m2theftguard/entity/ContactInfo.java
+++ app/src/main/java/cn/edu/gdmec/w07150837/myguard/m2theftguard/entity/ContactInfo.java
@@ -1,15 +1,7 @@
 package cn.edu.gdmec.w07150837.myguard.m2theftguard.entity;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-import cn.edu.gdmec.w07150837.myguard.R;
-
-public class ContactInfo extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_contact_info);
-    }
+public class ContactInfo{
+    public String id;
+    public String name;
+    public String phone;
 }
