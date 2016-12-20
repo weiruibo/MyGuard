9846a051e017ea7f759eefca7d7417f89c30c520
weiruibo <609044092@qq.com>
2016-12-20 22:24:53 +0800

    updata xml

--- app/src/main/java/cn/edu/gdmec/w07150837/myguard/m2theftguard/SetUp2Activity.java
+++ app/src/main/java/cn/edu/gdmec/w07150837/myguard/m2theftguard/SetUp2Activity.java
@@ -1,15 +1,82 @@
 package cn.edu.gdmec.w07150837.myguard.m2theftguard;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 import cn.edu.gdmec.w07150837.myguard.R;
 
-public class SetUp2Activity extends AppCompatActivity {
+public class SetUp2Activity extends BaseSetUpActivity implements View.OnClickListener {
+    private TelephonyManager mTelephonyManager;
+    private Button mBindSIMBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_up2);
+        mTelephonyManager = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+        initView();
+    }
+
+    private void initView() {
+        ((RadioButton) findViewById(R.id.rb_second)).setChecked(true);
+        mBindSIMBtn = (Button) findViewById(R.id.btn_bind_sim);
+        mBindSIMBtn.setOnClickListener(this);
+        if (isBind()) {
+            mBindSIMBtn.setEnabled(false);
+        } else {
+            mBindSIMBtn.setEnabled(true);
+        }
+    }
+
+    private boolean isBind() {
+        String simString = sp.getString("sim", null);
+        if (TextUtils.isEmpty(simString)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void showNext() {
+        if (!isBind()) {
+            Toast.makeText(this, "您还没有绑定SIM卡！", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        startActivityAndFinishSelf(SetUp3Activity.class);
+    }
+
+    @Override
+    public void showPre() {
+        startActivityAndFinishSelf(SetUp1Activity.class);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_bind_sim:
+                bindSIM();
+                break;
+        }
+    }
+
+    private void bindSIM() {
+        if (!isBind()) {
+            String simSerialNumber = mTelephonyManager.getSimSerialNumber();
+            SharedPreferences.Editor edit = sp.edit();
+            edit.putString("sim", simSerialNumber);
+            edit.commit();
+            Toast.makeText(this, "SIM卡绑定成功！", Toast.LENGTH_SHORT).show();
+            mBindSIMBtn.setEnabled(false);
+        } else {
+            Toast.makeText(this, "SIM卡已经绑定！", Toast.LENGTH_SHORT).show();
+            mBindSIMBtn.setEnabled(false);
+        }
     }
 }
--- app/src/main/java/cn/edu/gdmec/w07150837/myguard/m2theftguard/dialog/InterPasswordDialog.java
+++ app/src/main/java/cn/edu/gdmec/w07150837/myguard/m2theftguard/dialog/InterPasswordDialog.java
@@ -19,48 +19,57 @@ public class InterPasswordDialog extends Dialog implements android.view.View.OnC
     private Button mCancleBtn;
     private MyCallBack myCallBack;
     private Context context;
+
     public InterPasswordDialog(Context context) {
-        super(context,R.style.dialog_custom);
-        this.context=context;
+        super(context, R.style.dialog_custom);
+        this.context = context;
     }
-    public void setCallBack(MyCallBack myCallBcak){
-        this.myCallBack=myCallBcak;
+
+    public void setCallBack(MyCallBack myCallBcak) {
+        this.myCallBack = myCallBcak;
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.inter_password_dialog);
         initView();
     }
-    private void initView(){
-        mTitleTV =(TextView) findViewById(R.id.tv_interpwd_title);
+
+    private void initView() {
+        mTitleTV = (TextView) findViewById(R.id.tv_interpwd_title);
         mInterET = (EditText) findViewById(R.id.et_inter_password);
         mOKBtn = (Button) findViewById(R.id.btn_comfirm);
         mCancleBtn = (Button) findViewById(R.id.btn_dismiss);
         mOKBtn.setOnClickListener(this);
         mCancleBtn.setOnClickListener(this);
     }
-    public void setTitle(String title){
+
+    public void setTitle(String title) {
         if (!TextUtils.isEmpty(title)) {
             mTitleTV.setText(title);
         }
     }
-    public String getPassword(){
+
+    public String getPassword() {
         return mInterET.getText().toString();
     }
+
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.btn_comfirm:
-            myCallBack.confirm();
-            break;
+                myCallBack.confirm();
+                break;
             case R.id.btn_dismiss:
-            myCallBack.cancle();
-            break;
+                myCallBack.cancle();
+                break;
         }
     }
-    public interface MyCallBack{
+
+    public interface MyCallBack {
         void confirm();
+
         void cancle();
     }
 }
