fb2efb0a7f7aa4d83f94fbf6d8962a7748b4370d
weiruibo <609044092@qq.com>
2016-12-21 16:07:04 +0800

    add m6 allxml

---
+++ app/src/main/java/cn/edu/gdmec/w07150837/myguard/m6cleancache/adapter/CacheCleanAdapter.java
@@ -0,0 +1,8 @@
+package cn.edu.gdmec.w07150837.myguard.m6cleancache.adapter;
+
+/**
+ * Created by student on 16/12/21.
+ */
+
+public class CacheCleanAdapter {
+}
---
+++ app/src/main/java/cn/edu/gdmec/w07150837/myguard/m6cleancache/entity/CacheInfo.java
@@ -0,0 +1,8 @@
+package cn.edu.gdmec.w07150837.myguard.m6cleancache.entity;
+
+/**
+ * Created by student on 16/12/21.
+ */
+
+public class CacheInfo {
+}
