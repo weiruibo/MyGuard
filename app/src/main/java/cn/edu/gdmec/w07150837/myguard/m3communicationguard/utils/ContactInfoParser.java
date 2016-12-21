801593aababc0ac3317c82e6116af36ddc73012e
gdmec07150825 <lyx0008@2980.com>
2016-12-21 13:21:43 +0800

    lyx-utils

--- app/src/main/java/cn/edu/gdmec/w07150837/myguard/m3communicationguard/utils/ContactInfoParser.java
+++ app/src/main/java/cn/edu/gdmec/w07150837/myguard/m3communicationguard/utils/ContactInfoParser.java
@@ -1,22 +1,79 @@
 package cn.edu.gdmec.w07150837.myguard.m3communicationguard.utils;
 
-import android.os.Handler;
-import android.os.Message;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.ListView;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.text.TextUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import cn.edu.gdmec.w07150837.myguard.m2theftguard.ContactSelectActivity;
-import cn.edu.gdmec.w07150837.myguard.m2theftguard.adapter.ContactAdapter;
 import cn.edu.gdmec.w07150837.myguard.m2theftguard.entity.ContactInfo;
 
 /**
  * Created by weiruibo on 12/20/16.
  */
 
-public class ContactInfoParser  {
+public class ContactInfoParser {
 
+    public static List<ContactInfo> getSystemContact(Context context) {
+        ContentResolver resolver = context.getContentResolver();
+        Uri uri = Uri.parse("content://com.android.contacts/raw_contacts");
+        Uri datauri = Uri.parse("content://com.android.contacts/data");
+        List<ContactInfo> infos = new ArrayList<ContactInfo>();
+        Cursor cursor = resolver.query(uri, new String[]{"contact_id"}, null, null, null);
+        while (cursor.moveToNext()) {
+            String id = cursor.getString(0);
+            if (id != null) {
+                System.out.println("联系人id:" + id);
+                ContactInfo info = new ContactInfo();
+                info.id = id;
+                Cursor dataCursor = resolver.query(datauri, new String[]{"datal", "mimetype"},
+                        "raw_contact_id=?", new String[]{id}, null);
+                while (dataCursor.moveToNext()) {
+                    String datal = dataCursor.getString(0);
+                    String mimetype = dataCursor.getString(1);
+                    if ("vnd.android.cursor.item/name".equals(mimetype)) {
+                        System.out.println("姓名=" + datal);
+                        info.name = datal;
+                    } else if ("vnd.android.cursor.item/phone_v2".equals(mimetype)) {
+                        System.out.println("电话=" + datal);
+                        info.phone = datal;
+                    }
+
+                }
+
+                if (TextUtils.isEmpty(info.name) && TextUtils.isEmpty(info.phone))
+                    continue;
+                infos.add(info);
+                dataCursor.close();
+
+            }
+        }
+        cursor.close();
+        return infos;
+
+    }
+
+    public static List<ContactInfo> getSimContacts(Context context) {
+
+        Uri uri = Uri.parse("content://icc/adn");
+        List<ContactInfo> infos = new ArrayList<ContactInfo>();
+        Cursor mCursor = context.getContentResolver().query(uri, null, null, null
+                , null);
+        if (mCursor != null) {
+            while (mCursor.moveToNext()) {
+                ContactInfo info = new ContactInfo();
+                int nameFieldColumnIndex = mCursor.getColumnIndex("name");
+                info.name = mCursor.getString(nameFieldColumnIndex);
+                int numberFileColumnIndex = mCursor.getColumnIndex("number");
+                info.phone = mCursor.getString(numberFileColumnIndex);
+                infos.add(info);
+            }
+        }
+        mCursor.close();
+        return infos;
+    }
 
 }
