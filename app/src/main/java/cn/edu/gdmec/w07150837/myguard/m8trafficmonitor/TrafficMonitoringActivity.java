4469673e4bee1e972d79f6663c6dec41faa3b1b6
weiruibo <609044092@qq.com>
2016-12-22 15:31:22 +0800

    add m7

---
+++ app/src/main/java/cn/edu/gdmec/w07150837/myguard/m8trafficmonitor/TrafficMonitoringActivity.java
@@ -0,0 +1,15 @@
+package cn.edu.gdmec.w07150837.myguard.m8trafficmonitor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.edu.gdmec.w07150837.myguard.R;
+
+public class TrafficMonitoringActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_traffic_monitoring);
+    }
+}
