746d2b84878657f4ed965172df41ba58ad02ee9c
gdmec07150803 <1002471070@qq.com>
2016-12-23 09:26:27 +0800

    07150803

--- app/src/main/java/cn/edu/gdmec/w07150837/myguard/m7processmanager/service/AutoKillProcessService.java
+++ app/src/main/java/cn/edu/gdmec/w07150837/myguard/m7processmanager/service/AutoKillProcessService.java
@@ -1,16 +1,43 @@
 package cn.edu.gdmec.w07150837.myguard.m7processmanager.service;
 
+import android.app.ActivityManager;
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.IBinder;
 
 public class AutoKillProcessService extends Service {
-    public AutoKillProcessService() {
-    }
+    private ScreenLockReceiver receiver;
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return null;
+    }
+
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        receiver=new ScreenLockReceiver();
+        registerReceiver(receiver,new IntentFilter(Intent.ACTION_SCREEN_OFF));
+    }
+
+    @Override
+    public void onDestroy(){
+        unregisterReceiver(receiver);
+        receiver=null;
+        super.onDestroy();
+    }
+    class ScreenLockReceiver extends BroadcastReceiver{
+
+        @Override
+        public void onReceive(Context context,Intent intent){
+            ActivityManager am=(ActivityManager)getSystemService(ACTIVITY_SERVICE);
+            for(ActivityManager.RunningAppProcessInfo info:am.getRunningAppProcesses()){
+                String packname=info.processName;
+                am.killBackgroundProcesses(packname);
+            }
+        }
     }
 }
