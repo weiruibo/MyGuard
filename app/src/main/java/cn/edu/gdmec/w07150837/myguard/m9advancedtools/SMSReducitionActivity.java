45a778bd7f7917c87bc20b62262d5a52934c9f11
gdmec07150835 <853671221@qq.com>
2016-12-24 19:44:37 +0800

    m9 SMSRuducitionActivity

--- app/src/main/java/cn/edu/gdmec/w07150837/myguard/m9advancedtools/SMSReducitionActivity.java
+++ app/src/main/java/cn/edu/gdmec/w07150837/myguard/m9advancedtools/SMSReducitionActivity.java
@@ -2,14 +2,83 @@ package cn.edu.gdmec.w07150837.myguard.m9advancedtools;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
 
 import cn.edu.gdmec.w07150837.myguard.R;
+import cn.edu.gdmec.w07150837.myguard.m9advancedtools.utils.UIUtils;
+import cn.edu.gdmec.w07150837.myguard.m9advancedtools.widget.MyCircleProgress;
 
-public class SMSReducitionActivity extends AppCompatActivity {
-
+/*public class SMSReducitionActivity extends AppCompatActivity implements View.OnClickListener{
+    private MyCircleProgress mProgressButton;
+    private boolean flag=false;
+    private SmsReducitionUtils smsReducitionUtils;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_smsreducition);
+        initview();
+        SmsReducitionUtils=new SmsReducitionUuils();
     }
-}
+    private void initview(){
+        findViewById(R.id.rl_titleebar).setBackgroundColor(
+                getResources().getColor(R.color.bright_red);
+        ImageView mLeftImgv=(ImageView) findViewById(R.id.imgv_leftbtn);
+        ((TextView)findViewById(R.id.tv_title)).setText("短信还原");
+        mLeftImgv.setOnClickListener(this);
+        mLeftImgv.setImageResource(R.drawable.back);
+        mProgressButton=(MyCircleProgress)findViewById(R.id.mcp_reducition);
+        mProgressButton.setOnClickListener(this);
+    }
+    protected  void onDestroy(){
+        flag=false;
+        smsReducitionUtils.setFlag(flag);
+        super.onDestroy();
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.imgv_leftbtn:
+                finish();
+                break;
+            case R.id.mcp_reducition:
+                if(flag){
+                    flag=false;
+                    mProgressButton.setText("一键还原");
+                }else {
+                    flag=true;
+                    mProgressButton.setText("取消还原");
+                }
+                smsReducitionUtils.setFlag(flag);
+                new Thread(){
+                    public void run(){
+                        try{
+                            smsReducitionUtils.reducitionSms(SMSReducitionActivity.this,new
+                            SmsReducitionUtils.SmsRducitionCallBack(){
+                                public void onSmsReducition(int process){
+                                    mProgressButton.setProcess(process);
+                                }
+                                public void beforeSmsReducition(int size){
+                                    mProgressButton.setMax(size);
+                                }
+                            });
+                        }catch (XmlPullParserException e){
+                            e.printStackTrace();
+                            UIUtils.showToast(SMSReducitionActivity.this,"文件格式错误");
+                        }catch (IOException e){
+                            e.printStackTrace();
+                            UIUtils.showToast(SMSReducitionActivity.this,"读写错误");
+                        }
+                    }
+                }.start();
+                break;
+        }
+    }
+}*/
