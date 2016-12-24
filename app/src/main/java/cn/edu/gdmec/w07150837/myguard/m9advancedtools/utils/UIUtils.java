ee2927bc2a72303f9793810514f44a389a557b4b
gdmec07150835 <853671221@qq.com>
2016-12-24 13:33:32 +0800

    m9 utils UIUtils

--- app/src/main/java/cn/edu/gdmec/w07150837/myguard/m9advancedtools/utils/UIUtils.java
+++ app/src/main/java/cn/edu/gdmec/w07150837/myguard/m9advancedtools/utils/UIUtils.java
@@ -1,8 +1,23 @@
 package cn.edu.gdmec.w07150837.myguard.m9advancedtools.utils;
 
+import android.app.Activity;
+import android.widget.Toast;
+
 /**
  * Created by weiruibo on 12/23/16.
  */
 
 public class UIUtils {
+    public static void showToast(final Activity context,final String msg){
+        if ("main".equals(Thread.currentThread().getName())){
+            Toast.makeText(context,msg,Toast.LENGTH_LONG).show();
+        }else {
+            context.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(context,msg,Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+    }
 }
