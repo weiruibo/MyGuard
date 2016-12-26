61d3ae740770c79b7189797c0039e187fd904b45
gdmec07150825 <lyx0008@2980.com>
2016-12-26 10:54:33 +0800

    lyx-m9-db

--- app/src/main/java/cn/edu/gdmec/w07150837/myguard/m9advancedtools/db/AppLockOpenHelper.java
+++ app/src/main/java/cn/edu/gdmec/w07150837/myguard/m9advancedtools/db/AppLockOpenHelper.java
@@ -1,8 +1,30 @@
 package cn.edu.gdmec.w07150837.myguard.m9advancedtools.db;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by weiruibo on 12/23/16.
  */
 
-public class AppLockOpenHelper {
+public class AppLockOpenHelper extends SQLiteOpenHelper {
+
+
+    public AppLockOpenHelper(Context context) {
+        super(context, "applock.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("create table applock(id integer primary key autoincrement," +
+                "packagename varchar(20))");
+
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 }
--- app/src/main/java/cn/edu/gdmec/w07150837/myguard/m9advancedtools/db/dao/AppLockDao.java
+++ app/src/main/java/cn/edu/gdmec/w07150837/myguard/m9advancedtools/db/dao/AppLockDao.java
@@ -1,8 +1,84 @@
 package cn.edu.gdmec.w07150837.myguard.m9advancedtools.db.dao;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.edu.gdmec.w07150837.myguard.m9advancedtools.db.AppLockOpenHelper;
+
 /**
  * Created by weiruibo on 12/23/16.
  */
 
 public class AppLockDao {
+
+    private Context context;
+    private AppLockOpenHelper openHelper;
+    private Uri uri = Uri.parse("content://cn.edu.gdmec.w07150837.myguard.applock");
+
+    public AppLockDao(Context context) {
+        this.context = context;
+        openHelper = new AppLockOpenHelper(context);
+    }
+
+    public boolean insert(String packagename) {
+        SQLiteDatabase db = openHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("packagename", packagename);
+        long rowid = db.insert("applock", null, values);
+        if (rowid == -1) {
+            return false;
+        } else {
+            context.getContentResolver().notifyChange(uri, null);
+            return true;
+        }
+
+    }
+
+    public boolean delete(String packagname) {
+        SQLiteDatabase db = openHelper.getWritableDatabase();
+        int rownum = db.delete("applock", "packagname=?", new String[]{packagname});
+        if (rownum == 0) {
+            return false;
+        } else {
+            context.getContentResolver().notifyChange(uri, null);
+            return true;
+        }
+
+    }
+
+    public boolean find(String packagename) {
+        SQLiteDatabase db = openHelper.getReadableDatabase();
+        Cursor cursor = db.query("applock", null, "packagename=?", new String[]{packagename},
+                null, null, null);
+        if (cursor.moveToNext()) {
+            cursor.close();
+            db.close();
+            return true;
+        } else {
+            cursor.close();
+            return false;
+        }
+
+    }
+
+    public List<String> findAll() {
+        SQLiteDatabase db = openHelper.getReadableDatabase();
+        Cursor cursor = db.query("applock", null, null, null, null, null, null);
+        List<String> packages = new ArrayList<String>();
+        while (cursor.moveToNext()) {
+            String string = cursor.getString(cursor.getColumnIndex("packagename"));
+            packages.add(string);
+        }
+        return packages;
+
+
+    }
+
+
 }
